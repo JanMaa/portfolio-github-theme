95ca9bdc42f78167987f96deee704027ce64d2b6
Janric Malate <janricmalate@gmail.com>
2024-09-24 03:04:16 +0800

    added alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,5 @@
-// import path from "path"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   base: "/portfolio-github-theme/"
-// })
-
-
 // vite.config.js
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -21,6 +7,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
     base: '/',
   }
 
